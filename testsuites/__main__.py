731dda2bfcbc9e27d9ab256064a918217087566e
ownport <ownport@gmail.com>
2016-09-13 23:53:39 +0300

    [*] move the logic from __main__ to main.py

--- testsuites/__main__.py
+++ testsuites/__main__.py
@@ -1,14 +1,3 @@
+from main import run
 
-import sys
-import __init__
-
-import pytest
-from _pytest import config
-
-try:
-    args = sys.argv[1:]
-    pytest.main(args)
-except IOError, err:
-    print >> sys.stderr, "[ERROR] Input/Output error, %s" % err
-    sys.exit(1)
-
+run()
