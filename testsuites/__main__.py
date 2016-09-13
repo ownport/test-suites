b209195774d3a5c87d25f7919cc63845d5b3388b
ownport <ownport@gmail.com>
2016-09-14 00:13:23 +0300

    [*] fixed crosslink between modules

--- testsuites/__main__.py
+++ testsuites/__main__.py
@@ -1,3 +1,6 @@
+
+from __init__ import __version__
+
 from main import run
 
 run()
--- testsuites/main.py
+++ testsuites/main.py
@@ -1,14 +1,16 @@
 
-import sys
 import __init__
 
+import sys
 import pytest
-from _pytest import config
 
-try:
-    args = sys.argv[1:]
-    pytest.main(args)
-except IOError, err:
-    print >> sys.stderr, "[ERROR] Input/Output error, %s" % err
-    sys.exit(1)
+
+def run():
+
+    try:
+        args = sys.argv[1:]
+        pytest.main(args)
+    except IOError, err:
+        print >> sys.stderr, "[ERROR] Input/Output error, %s" % err
+        sys.exit(1)
 
