cbc89570b8818a1dda83d1eae4d7030d591b8a4b
ownport <ownport@gmail.com>
2016-11-11 00:22:44 +0200

    [*] updated python path

--- testsuites/__init__.py
+++ testsuites/__init__.py
@@ -5,4 +5,5 @@ import os
 import sys
 
 path = os.path.realpath(os.path.abspath(__file__))
+sys.path.insert(0, os.path.dirname(os.path.dirname(path)))
 sys.path.insert(0, os.path.join(os.path.dirname(path), 'packages/'))
