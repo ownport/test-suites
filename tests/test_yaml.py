808e05e2bb98f35b3af4f3715cca1f002749d8eb
ownport <ownport@gmail.com>
2016-11-11 00:23:07 +0200

    [*] fixed yaml tests

--- tests/test_yaml.py
+++ tests/test_yaml.py
@@ -1,4 +1,6 @@
 
+import testsuites
+
 test_yaml_file='''
 key1: value1
 key2: value2
@@ -6,5 +8,5 @@ key2: value2
 
 def test_yaml_pkg_import():
 
-    from testsuites.packages import yaml
+    import yaml
     assert yaml.load(test_yaml_file) == {'key1': 'value1', 'key2': 'value2'}
