8288fa4a88e1cef95a6cc82bd108689b4aff38de
ownport <ownport@gmail.com>
2016-11-11 00:08:50 +0200

    [*] v0.1.2

--- testsuites/__init__.py
+++ testsuites/__init__.py
@@ -1,9 +1,8 @@
 __title__ = 'test-suites'
-__version__="0.1.1"
+__version__="0.1.2"
 
 import os
 import sys
 
 path = os.path.realpath(os.path.abspath(__file__))
 sys.path.insert(0, os.path.join(os.path.dirname(path), 'packages/'))
-
