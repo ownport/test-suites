8f03ddc245e8e5605dd8768340d5c11b133ee812
ownport <ownport@gmail.com>
2017-03-27 07:02:03 +0300

    [+] py3 support

--- testsuites/main.py
+++ testsuites/main.py
@@ -1,16 +1,15 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
 
-import __init__
-
+import os
 import sys
 import pytest
 
 
 def run():
-
     try:
         args = sys.argv[1:]
         pytest.main(args)
-    except IOError, err:
+    except IOError as err:
         print >> sys.stderr, "[ERROR] Input/Output error, %s" % err
         sys.exit(1)
-
