7fd5b2fa6b4b814870256e7feab8f9827bcc861f
ownport <ownport@gmail.com>
2017-04-11 22:44:12 +0300

    [+] pytest-cov plugin

--- testsuites/main.py
+++ testsuites/main.py
@@ -5,11 +5,12 @@ import os
 import sys
 import pytest
 
+PYTEST_PLUGINS='pytest_cov.plugin'.split()
 
 def run():
     try:
         args = sys.argv[1:]
-        pytest.main(args)
+        pytest.main(args, plugins=PYTEST_PLUGINS)
     except IOError as err:
         print >> sys.stderr, "[ERROR] Input/Output error, %s" % err
         sys.exit(1)
